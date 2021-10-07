c6e3d13bf18bcf7a116bb915a1467e256bef6d3e
Gülay Şahin (ETIYA) <gulay.sahin@etiya.com>
2021-10-07 10:30:09 +0300

    ReCapProject

---
+++ reCapProject/reCapProject/src/main/java/com/etiya/reCapProject/business/paths/Paths.java
@@ -0,0 +1,6 @@
+package com.etiya.reCapProject.business.paths;
+
+public class Paths {
+	public static String CAR_IMAGE_DEFAULT_PATH = "C:\\java_Projects\\reCapProject\\reCapProject\\reCapProject\\images\\default.png";
+	public static String CAR_IMAGES_PATH="C:\\java_Projects\\reCapProject\\reCapProject\\reCapProject\\images";
+}
---
+++ reCapProject/reCapProject/src/main/java/com/etiya/reCapProject/entities/requests/careRequest/DeleteCareRequest.java
@@ -0,0 +1,22 @@
+package com.etiya.reCapProject.entities.requests.careRequest;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeleteCareRequest {
+	
+	@NotNull(message = "Boş Geçilemez!")
+	private int carId;
+
+	@NotNull(message = "Boş Geçilemez!")
+	private int careId;
+	
+}
